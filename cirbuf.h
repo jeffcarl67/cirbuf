1185d26771378e5078bab61b56a09f636dfad958
jeffcarl67 <jeffcarljeffcarl@gmail.com>
2019-05-27 19:40:22 +0800

    Add missing codes

--- cirbuf.h
+++ cirbuf.h
@@ -85,6 +85,7 @@ static inline int cirbuf_offer(cirbuf_t *cb,
     memcpy(cb->data + cb->tail, data, written);
     cb->tail += written;
     /* TODO: add your code here */
+    if (cb->size < cb->tail) cb->tail %= cb->size;
     return written;
 }
 
@@ -108,7 +109,7 @@ static inline unsigned char *cirbuf_peek(const cirbuf_t *cb)
         return NULL;
 
     /* TODO: add your own code here */
-    return NULL;
+    return cb->data + cb->head;
 }
 
 /** Release data at the head from the circular buffer.
